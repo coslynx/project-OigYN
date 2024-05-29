ebbf37fb715b3bc9aa1c4e2594bd9df4c4477c04
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 18:59:10 +0530

    Added: `discord-moderation-bot/utils/userActivityTracker.js`

---
+++ discord-moderation-bot/utils/userActivityTracker.js
@@ -0,0 +1,13 @@
+const userActivityTracker = {
+  trackUserActivity: (userId, activityType) => {
+    // Logic to track user activity
+  },
+  getUserActivity: (userId) => {
+    // Logic to get user activity
+  },
+  clearUserActivity: (userId) => {
+    // Logic to clear user activity
+  },
+};
+
+module.exports = userActivityTracker;
